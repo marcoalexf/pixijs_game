057011f3623a3e47441e72cc1e41b3657a8476c5
Marco Alexandre dos Santos Francisco <marcoasfran@gmail.com>
2021-09-06 23:53:04 +0100

    [feature] particle: added particle system

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,63 @@
 import * as PIXI from 'pixi.js'
+import * as particles from '@pixi/particle-emitter'
+
+const particleConfig =
+{
+  alpha: {
+		start: 1,
+		end: 1
+	},
+	scale: {
+		start: 0.1,
+		end: 0.01,
+		minimumScaleMultiplier: 1
+	},
+	color: {
+		start: "#ffffff",
+		end: "#ff0000"
+	},
+	speed: {
+		start: 200,
+		end: 50,
+		minimumSpeedMultiplier: 1
+	},
+	acceleration: {
+		x: 0,
+		y: 0
+	},
+	maxSpeed: 0,
+	startRotation: {
+		min: 0,
+		max: 360
+	},
+	noRotation: false,
+	rotationSpeed: {
+		min: 0,
+		max: 0
+	},
+	lifetime: {
+		min: 0.001,
+		max: 1
+	},
+	blendMode: 'screen',
+	frequency: 0.001,
+	emitterLifetime: -1,
+	maxParticles: 1000,
+	pos: {
+		x: 200,
+		y: 200
+	},
+	addAtBack: false,
+	spawnType: 'point',
+  behaviors: [
+      {
+          type: 'textureSingle',
+          config: {
+              texture: PIXI.Texture.from('assets/particle.png')
+          }
+      }
+  ],
+}
 
 const loadAssets = (filenames) => {
   const textures = {};
@@ -13,16 +72,29 @@ const loadAssets = (filenames) => {
   return sprites;
 }
 
-const draw = (app, loadedAssets) => {
-  const { particle, fountain, rocket } = loadedAssets;
+const loadParticles = (container) => {
+  debugger;
+  const emitter = ['test_particle', new particles.Emitter(container, particleConfig)];
+
+  return {
+    [emitter[0]]: emitter[1]
+  }
+}
+
+const draw = (app) => {
+  // load assets
+  const assets = ['fountain.png', 'rocket.png'];
+  const loadedAssets = loadAssets(assets);
+  const { fountain, rocket } = loadedAssets;
+
+  // create container for particles, add it to main stage and load particles into it
+  const container = new PIXI.ParticleContainer();
+  app.stage.addChild(container);
+  const loadedParticles = loadParticles(container);
 
   fountain.anchor.set(0.5)
   fountain.x = 200
   fountain.y = 200
-
-  particle.anchor.set(0.5)
-  particle.x = 250
-  particle.y = 250
 
   rocket.anchor.set(0.5)
   rocket.x = 300
@@ -32,33 +104,46 @@ const draw = (app, loadedAssets) => {
     const asset = loadedAssets[key];
     app.stage.addChild(asset)
   });
-   // app.stage.addChild(particle)
+
+  return {
+    loadedAssets,
+    loadedParticles
+  }
 }
 
-const update = (loadedAssets, delta) => {
-  const { particle, fountain, rocket } = loadedAssets;
-  particle.rotation -= 0.01 * delta;
+const update = (loadedAssets, loadedParticles, delta, time) => {
+  const { fountain, rocket } = loadedAssets;
   fountain.rotation -= 0.01 * delta;
   rocket.rotation -= 0.01 * delta;
+
+  const { test_particle } = loadedParticles;
+  const now = Date.now()
+	test_particle.update((now - time.elapsed) * 0.001);
+  time.updateElapsedTime(now);
 }
 
 const run = (app) => {
-  const assets = ['particle.png', 'fountain.png', 'rocket.png'];
-  const loadedAssets = loadAssets(assets);
-
+  let elapsed = Date.now();
+  const time = {
+    elapsed,
+    updateElapsedTime: function (updatedTime) {
+      this.elapsed = updatedTime;
+    }
+  }
   // draw resources
-  draw(app, loadedAssets);
+  const {loadedAssets, loadedParticles} = draw(app);
+  loadedParticles['test_particle'].emit = true;
   // start the game loop
   app.ticker.add((delta) => {
-    update(loadedAssets, delta);
+    update(loadedAssets, loadedParticles, delta, time);
   });
 }
 
 
 const app = new PIXI.Application({
-  width: 720,
-  height: 1280,
-  backgroundColor: 0x1099bb,
+  width: 860,
+  height: 640,
+  backgroundColor: 'black',
   view: document.querySelector('#scene')
 });
 
